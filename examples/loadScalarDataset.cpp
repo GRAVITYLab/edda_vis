f1c55b1a8b8c90dd97f2bc98e20c92986fd7d966
Chun-Ming Chen <chenchu@cse.ohio-state.edu>
2016-06-18 22:41:04 -0400

    add a scalar loader example

---
+++ examples/loadScalarDataset.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include "edda.h"
+#include "distributions/gaussian.h"
+#include "distributions/distribution.h"
+#include "io/file_reader.h"
+#include "filters/stream_tracer.h"
+#include "dataset/dataset.h"
+#include "io/edda_reader.h"
+
+using namespace std;
+using namespace edda;
+
+int main(int argc, char **argv)
+{
+  srand(time(NULL));  // random seeding
+
+  cout << "Loading sample file" << endl;
+  string filename = SAMPLE_DATA_PATH;
+  filename = filename + "/out_92651_0.vts";
+
+  // load data with random sampling
+  shared_ptr<Dataset<Real> > dataset = loadEddaScalarDataset(filename, "");
+
+  VECTOR3 pos;
+  Real value;
+
+  pos = VECTOR3(10,10,10);
+  dataset->at_phys(pos, value);
+  cout << pos << ": " << value << endl;
+
+
+  pos = VECTOR3(2.1,2.1,2.1);
+  dataset->at_phys(pos, value);
+  cout << pos << ": " << value << endl;
+
+  return 0;
+}
