e217ee2d541eff756727d02da1dc2e04e1e8c24d
ChengLiOSU <li.4076@osu.edu>
2016-06-20 15:12:06 -0400

    fit for Windows update
    adding EDDA_EXPORT for a few functions. Now works on VS2012 with both
    shared lib turned on/off. But only works on Release mode

--- src/io/edda_reader.h
+++ src/io/edda_reader.h
@@ -2,13 +2,14 @@
 #define EDDA_READER
 
 #include <string>
+#include "edda_export.h"
 #include "dataset/dataset.h"
 
 namespace edda{
 
 
-  std::shared_ptr<Dataset<Real> > loadEddaScalarDataset(const std::string &edda_file, const std::string &array_name="");
-  std::shared_ptr<Dataset<VECTOR3> > loadEddaVector3Dataset(const std::string &edda_file, const std::string &array_name="");
+	std::shared_ptr<Dataset<Real> > EDDA_EXPORT loadEddaScalarDataset(const std::string &edda_file, const std::string &array_name = "");
+	std::shared_ptr<Dataset<VECTOR3> > EDDA_EXPORT loadEddaVector3Dataset(const std::string &edda_file, const std::string &array_name = "");
 
 }
 
